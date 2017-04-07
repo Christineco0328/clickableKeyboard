2dca5cb451c907765bc7acffe4030466a4c6cdd6
Christine Co <cco.sh@mysummitps.org>
2017-04-07 20:11:27 +0000

    update

--- keyboard.js
+++ keyboard.js
@@ -1,3 +1,5 @@
+//Here are the variables that connect the elements in the html and carries them over the javascript
+
 var c1 = document.getElementById("C1");
 var d = document.getElementById("D");
 var E = document.getElementById("E");
@@ -14,7 +16,7 @@ var songTitle = document.getElementById("songTitle");
 
 
 
-//global data
+//global data; these are the variables that contain audio files of each note
 var audioC1 = new Audio('203485__tesabob2001__c5.mp3');
 var audioD = new Audio('203473__tesabob2001__d5.mp3');
 var audioE = new Audio('203476__tesabob2001__e5.mp3');
@@ -24,67 +26,57 @@ var audioA = new Audio('203464__tesabob2001__a5.mp3');
 var audioB = new Audio('203467__tesabob2001__b5.mp3');
 var audioC2 = new Audio('203484__tesabob2001__c6.mp3');
 
-
-
-
-
-// twinkle = [];
-// twinkle.push(audioC1.play());
-// twinkle.push(audioD.play());
-// twinkle.push(audioE.play());
-// twinkle.push(audioF.play());
-// twinkle.push(audioG.play());
-// twinkle.push(audioA.play());
-// twinkle.push(audioB.play());
-// twinkle.push(audioC2.play());
-
+//this is the add event listener that allows the program to react to when a key on the keyboard is pressed; it associates the audio file with a set key on the keyboard using its keycode. If true, it will play the first "C" audio file
 document.addEventListener("keydown", function(e) {
     if (e.keyCode === 65) {
         audioC1.play();
+//This part of the code controls the css that is changed when a key is press on the keyboard
         c1.style.backgroundColor = "#33ccff";
         c1.style.color = "white";
         c1.style.transform = "scale(.85,.85)";
-
+//The timeout method tells the program to revert back to the css of the element in mainstyle after a set amount of time. Because the quotation marks are empty, the program looks to mainstyle to find what the css for the element should look like 
         setTimeout(function() {
             c1.style.backgroundColor = "";
             c1.style.color = "";
             c1.style.transform = "";
         }, 200);
     }
+//If the last if statement is false, it will check to see if this key is pressed based on key code. If true it will play the audio file for the note "D"    
     if (e.keyCode === 83) {
         audioD.play();
-
+//css that changes if the key pressed is "D"
         d.style.backgroundColor = "#00e600";
         d.style.color = "white";
         d.style.transform = "scale(.85,.85)";
-
+//This method tells the function to go back to the default css in mainstyle after a set amount of time.
         setTimeout(function() {
             d.style.backgroundColor = "";
             d.style.color = "";
             d.style.transform = "";
         }, 200);
     }
-
+//If this if statement is true, it will play the "E" audio file
     if (e.keyCode === 68) {
         audioE.play();
-
+//css that changes when "E" is pressed
         E.style.backgroundColor = "#ffff00";
         E.style.color = "white";
         E.style.transform = "scale(.85,.85)";
-
+//reverts code back to mainstyle        
         setTimeout(function() {
             E.style.backgroundColor = "";
             E.style.color = "";
             E.style.transform = "";
         }, 200);
     }
-
+//If true, it will play the audio file for "F"
     if (e.keyCode === 70) {
         audioF.play();
 
         f.style.backgroundColor = "#ff6600";
         f.style.color = "white";
         f.style.transform = "scale(.85,.85)";
+//reverts code back to mainstyle        
 
         setTimeout(function() {
             f.style.backgroundColor = "";
@@ -92,6 +84,7 @@ document.addEventListener("keydown", function(e) {
             f.style.transform = "";
         }, 200);
     }
+//If true, it will play the audio file for "G"
 
     if (e.keyCode === 74) {
         audioG.play();
@@ -99,6 +92,7 @@ document.addEventListener("keydown", function(e) {
         g.style.backgroundColor = "#ff4d4d";
         g.style.color = "white";
         g.style.transform = "scale(.85,.85)";
+//reverts code back to mainstyle        
 
         setTimeout(function() {
             g.style.backgroundColor = "";
@@ -106,6 +100,7 @@ document.addEventListener("keydown", function(e) {
             g.style.transform = "";
         }, 200);
     }
+//If true, it will play the audio file for "A"
 
     if (e.keyCode === 75) {
         audioA.play();
@@ -113,6 +108,7 @@ document.addEventListener("keydown", function(e) {
         a.style.backgroundColor = "#e600ac";
         a.style.color = "white";
         a.style.transform = "scale(.85,.85)";
+//reverts code back to mainstyle        
 
         setTimeout(function() {
             a.style.backgroundColor = "";
@@ -120,6 +116,7 @@ document.addEventListener("keydown", function(e) {
             a.style.transform = "";
         }, 200);
     }
+//If true, it will play the audio file for "B"
 
     if (e.keyCode === 76) {
         audioB.play();
@@ -127,6 +124,7 @@ document.addEventListener("keydown", function(e) {
         b.style.backgroundColor = "#800080";
         b.style.color = "white";
         b.style.transform = "scale(.85,.85)";
+//reverts code back to mainstyle        
 
         setTimeout(function() {
             b.style.backgroundColor = "";
@@ -134,6 +132,7 @@ document.addEventListener("keydown", function(e) {
             b.style.transform = "";
         }, 200);
     }
+//If true, it will play the audio file for "C2"
 
     if (e.keyCode === 186) {
         audioC2.play();
@@ -141,6 +140,7 @@ document.addEventListener("keydown", function(e) {
         c2.style.backgroundColor = "#000099";
         c2.style.color = "white";
         c2.style.transform = "scale(.85,.85)";
+//reverts code back to mainstyle        
 
         setTimeout(function() {
             c2.style.backgroundColor = "";
@@ -184,53 +184,36 @@ c2.addEventListener("click", function() {
     audioC2.play();
 });
 
-
+//This part of the code controls the innerHTML of the two divs for the title of the song and the notes for the song when a button is pressed.
+//Add event listener checks to see if button one is clicked; When button 1 is pressed, it will display the title "Twinkle Twinkle Little Star" and show the notes to play it in the bottom div
 button1.addEventListener("click", function() {
+//div for song title     
     songTitle.innerHTML = "<h1 style='font-size:30px'> Twinkle Twinkle Little Star </h1>";
-
+    
+//change in HTML for the div that displays the notes
     notes.innerHTML = " <p style='color:#33ccff'> C C</p> <p style='color:#ff4d4d'> G G </p> <p style='color:#e600ac'> A A </p> <p style='color:#ff4d4d'> G </p> <p style='color:#ff6600'> F F</p> <p style='color:#ffff00'> E E</p> <p style='color:#00e600'> D D</p><p style='color:#33ccff'> C </p> <br> <p style='color:#ff4d4d'> G G </p> <p style='color:#ff6600'> F F</p>  <p style='color:#ffff00'> E E</p> <p style='color:#00e600'> D </p> <br> <p style='color:#ff4d4d'> G G </p> <p style='color:#ff6600'> F F F</p> <p style='color:#ffff00'> E E</p> <p style='color:#00e600'> D </p> <br> <p style='color:#33ccff'> C C</p> <p style='color:#ff4d4d'> G G </p> <p style='color:#e600ac'> A A </p> <p style='color:#ff4d4d'> G </p> <p style='color:#ff6600'> F F</p> <p style='color:#ffff00'> E E</p> <p style='color:#00e600'> D D</p> <p style='color:#33ccff'> C </p>";
 
 });
 
+//add event listener listens for if the second button is pressed
 button2.addEventListener("click", function() {
+//if the button is pressed, the div for the song title will change to Mary Had a Little Lamb    
     songTitle.innerHTML = "<h1 style='font-size:30px'> Mary Had a Little Lamb </h1>";
-    
+//if the button is pressed, the div containing the notes will also change and display the notes for the song    
     notes.innerHTML = "<p style='color:#ffff00'> E </p> <p style='color:#00e600'> D</p> <p style='color:#33ccff'> C </p> <p style='color:#00e600'> D</p> <p style='color:#ffff00'> E E E </p> <br> <p style='color:#00e600'> D D D</p> <p style='color:#ffff00'> E </p> <p style='color:#ff4d4d'> G G </p> <br> <p style='color:#ffff00'> E </p> <p style='color:#00e600'> D</p>  <p style='color:#33ccff'> C </p> <p style='color:#00e600'> D</p> <p style='color:#ffff00'> E E E </p> <br> <p style='color:#ffff00'> E </p> <p style='color:#00e600'> D D </p> <p style='color:#ffff00'> E </p> <p style='color:#00e600'> D </p> <p style='color:#33ccff'> C </p>";
         
  
 });
    
   
-     
+//add event listener checks to see if the third button is pressed 
 
 button3.addEventListener("click", function() {
+    // if the button is pressed, the song title div innerHTML will change to Happy Birthday to You
     songTitle.innerHTML = "<h1 style='font-size:30px'> Happy Birthday To You </h1>";
-    
+    // if the button is pressed, the notes div HTML will change to display the notes for Happy Birthday to You
     notes.innerHTML = "<p style='color:#33ccff'> C C </p> <p style='color:#00e600'> D </p> <p style='color:#33ccff'> C </p> <p style='color:#ff6600'> F </p> <p style='color:#ffff00'> E , </p> <p style='color:#33ccff'> C C </p> <p style='color:#00e600'> D </p> <p style='color:#33ccff'> C </p><p style='color:#ff4d4d'> G </p> <p style='color:#ff6600'> F </p> <br> <p style='color:#33ccff'> C C </p><p style='color:#000099'> C </p> <p style='color:#e600ac'> A </p> <p style='color:#ff6600'> F </p> <p style='color:#ffff00'> E </p> <p style='color:#00e600'> D </p> <br> <p style='color: #800080'> B B </p> <p style='color:#e600ac'> A </p> <p style='color:#ff6600'> F </p> <p style='color:#ff4d4d'> G </p> <p style='color:#ff6600'> F </p> <p style='color:#33ccff'> C C </p> <br> <p style='color:#33ccff'> C C </p> <p style='color:#00e600'> D </p> <p style='color:#33ccff'> C </p> <p style='color:#ff6600'> F </p> <p style='color:#ffff00'> E ,</p> <p style='color:#33ccff'> C C </p> <p style='color:#00e600'> D </p> <p style='color:#33ccff'> C </p><p style='color:#ff4d4d'> G </p> <p style='color:#ff6600'> F </p> <br> <p style='color:#33ccff'> C C </p><p style='color:#000099'> C </p> <p style='color:#e600ac'> A </p> <p style='color:#ff6600'> F </p> <p style='color:#ffff00'> E </p> <p style='color:#00e600'> D ,</p> <p style='color: #800080'> B B </p> <p style='color:#e600ac'> A </p> <p style='color:#ff6600'> F </p> <p style='color:#ff4d4d'> G </p> <p style='color:#ff6600'> F </p> ";
 
 
     });
     
-    
-    
-    
-// <p style='color:#33ccff'> C </p>
-// <p style='color:#ff4d4d'> G  </p>
-// <p style='color:#e600ac'> A </p>
-// <p style='color:#ff4d4d'> G </p>
-// <p style='color:#ff6600'> F </p>
-// <p style='color:#ffff00'> E </p>
-// <p style='color:#00e600'> D </p>
-// <p style='color:#33ccff'> C </p>
-
-// <p style='color: #800080'> B </p>
-// <p style='color:#000099'> C </p>
-    //  var play = twinkle[0];    
-    //  for (i = 0;i < twinkle.length; i++) {
-    //      if(i === audioC1) {
-    //      play = twinkle[i];
-
-//      }
-//  }
-
-// })
